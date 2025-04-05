bf9c26da1ff86d252457d17b73e77f5e64125639
ysilvv <yasmim.silva@sptech.school>
2025-04-05 00:21:57 -0300

    add: implementação da tela de atendente

--- src/pages/Atendente/Atendente.jsx
+++ src/pages/Atendente/Atendente.jsx
@@ -1,9 +1,32 @@
 import './Atendente.css'
+import BotaoConfirmar from '../../components/Botoes/BotaoConfirmar/BotaoConfirmar'
+import ElementoTotal from '../../components/HoverTotalProduto/ElementoTotal'
 
 export function Atendente() {
-    return(
+    return (
         <>
-        <div>Olá, você está na tela de Atendente</div>
+            <section className="menu-atendente">
+                <h1>Escolha o Setor</h1>
+
+
+                <div className="setores">
+                    <ElementoTotal />
+                    <ElementoTotal />
+                    <ElementoTotal />
+                    <ElementoTotal />
+                </div>
+
+            </section>
+
+            <aside className="menu-comanda">
+                <header className="header-comanda">
+                    <h1>Comandas</h1>
+                </header>
+
+                <section className="botao-confirmar">
+                    <BotaoConfirmar />
+                </section>
+            </aside>
         </>
     )
 }

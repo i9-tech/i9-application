841a6fe525bf031230ee3a473f519bdd615da8e6
m4yk0n <maykon.rodrigues@sptech.school>
2025-04-09 20:30:07 -0300

    add: imagem dos botoes adicionada

--- src/components/OpcoesCardSobreposto/OpcoesCardSobreposto.jsx
+++ src/components/OpcoesCardSobreposto/OpcoesCardSobreposto.jsx
@@ -1,25 +1,29 @@
-import BotaoOpcaoHover from "../Hovers/HoverBTN/BotaoOpcaoHover";
+import BotaoOpcao from "../Hovers/HoverBotoes/BotaoOpcao";
+import ElementoImagem from "../Hovers/HoverImagem/ElementoImagem";
 
 export default function OpcoesCardSobreposto() {
   return (
     <>
       <article className="opcoes-desktop">
         <span className="options">
-          <button className="botao-opcao">Gestão de Comandas</button>
-          <button className="botao-opcao selecionado">
+          <button className="botao-option">Gestão de Comandas</button>
+          <button className="botao-option selecionado">
             Frente de Caixa PDV
           </button>
-          <button className="botao-opcao">Gestão de Estoque</button>
+          <button className="botao-option">Gestão de Estoque</button>
         </span>
         <span className="infos-sistema">
           <div className="botoes-opcoes">
-          <p>
-            O nosso sistema facilita o registro de pedidos e pagamentos,
-            garantindo um atendimento rápido, eficiente e sem complicações
-          </p>
-            <BotaoOpcaoHover texto={"Registro Simplificado de Pedidos"} />
-            <BotaoOpcaoHover texto={"Cardápio Personalizável"} />
-            <BotaoOpcaoHover texto={"Controle de Vendas"} />
+            <p>
+              O nosso sistema facilita o registro de pedidos e pagamentos,
+              garantindo um atendimento rápido, eficiente e sem complicações
+            </p>
+            <BotaoOpcao texto={"Registro Simplificado de Pedidos"} />
+            <BotaoOpcao texto={"Cardápio Personalizável"} />
+            <BotaoOpcao texto={"Controle de Vendas"} />
+          </div>
+          <div className="imagem-opcao">
+            <ElementoImagem />
           </div>
         </span>
       </article>

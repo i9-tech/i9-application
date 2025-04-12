4bfc550667dd5126f622ed6372c7ead24a87a128
m4yk0n <maykon.rodrigues@sptech.school>
2025-04-12 19:04:47 -0300

    feat: navbar institucional

---
+++ src/components/NavInstitucional/NavInstitucional.jsx
@@ -0,0 +1,17 @@
+export default function NavInstitucional() {
+  return (
+    <section className="nav-institucional">
+        <div>LOGO</div>
+        <span className="opcoes-nav">
+        <div>Início</div>
+        <div>Nossas Soluções</div>
+        <div>Sobre Nós</div>
+        <div>Contato</div>
+        </span>
+        <span className="entrar">
+        <div><hov>Entre em contato conosco</hov></div>
+        <div className="botao-enter"><button>Entrar ></button></div>
+        </span>
+    </section>
+    )
+}
--- src/pages/Institucional/Institucional.jsx
+++ src/pages/Institucional/Institucional.jsx
@@ -2,10 +2,12 @@ import CardSobreposto from '../../components/CardSobreposto/CardSobreposto';
 import './Institucional.css';
 import '../../index.css'
 import Perguntas from '../../components/Perguntas/Perguntas';
+import NavInstitucional from '../../components/NavInstitucional/NavInstitucional';
 
 export function Institucional() {
   return (
     <>
+    <NavInstitucional/>
     <section className='institucional'>
       <CardSobreposto/>
     </section>

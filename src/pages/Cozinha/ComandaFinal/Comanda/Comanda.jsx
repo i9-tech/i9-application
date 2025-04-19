a538aef09bdc41eae0561d7b772556a00026a04f
m4yk0n <maykon.rodrigues@sptech.school>
2025-04-19 18:36:42 -0300

    feat: funcoes na comanda / fix: css da comanda

--- src/pages/Cozinha/ComandaFinal/Comanda/Comanda.jsx
+++ src/pages/Cozinha/ComandaFinal/Comanda/Comanda.jsx
@@ -1,63 +1,39 @@
-import React from 'react'
-import ComandaHeader from '../ComandaHeader/ComandaHeader'
-import ComandaBody from '../ComandaBody/ComandaBody'
-import ComandaFooter from '../ComandaFooter/ComandaFooter'
+import React from "react";
+import ComandaHeader from "../ComandaHeader/ComandaHeader";
+import ComandaBody from "../ComandaBody/ComandaBody";
+import ComandaFooter from "../ComandaFooter/ComandaFooter";
 
-import { useState } from 'react';
+export default function Comanda({ pedido }) {
+  return (
+    <div className="comanda">
+      <div className="cabecalho-comanda">
+        <ComandaHeader
+          numeroPedido={pedido.numeroPedido}
+          dataHora={pedido.dataHora}
+        />
+      </div>
 
-export default function Comanda() {
-    const [pedido, setPedido] = useState({
-        numeroPedido: 250,
-        dataHora: '19 Mar 2025, 16:54',
-        itens: [
-            {
-                titulo: '1x Lanche Natural',
-                descricao: 'Pão, Alface, Tomate e Molho.',
-                observacao: 'SEM TOMATE'
-            },
-            {
-                titulo: '1x Lanche Natural',
-                descricao: 'Pão, Alface, Tomate e Molho.'
-            },
-            {
-                titulo: '1x Chicken Jr.',
-                descricao: 'Pão de hambúrguer, frango empanado, queijo, presunto...'
-            },
-            {
-                titulo: '1x Lanche Natural',
-                descricao: 'Pão, Alface, Tomate e Molho.'
-            },,
-            {
-                titulo: '1x Lanche Natural',
-                descricao: 'Pão, Alface, Tomate e Molho.'
-            },
-        ],
-        qtdItens: 3
-    });
+      <div className="corpo-comanda">
+        {pedido.itens.map((item, index) => (
+          <ComandaBody
+            key={index}
+            titulo={
+              <>
+                <span>{item.titulo.split(" ")[0]}</span>{" "}
+                {item.titulo.split(" ").slice(1).join(" ")}
+              </>
+            }
+            descricao={item.descricao}
+            observacao={item.observacao}
+            index={index}
+            pedidoId={pedido.numeroPedido}
+          />
+        ))}
+      </div>
 
-    return (
-        <>
-            <div className="comanda">
-                <div className="cabecalho-comanda">
-                    <ComandaHeader />
-                </div>
-
-                <div className="corpo-comanda">
-                    {pedido.itens.map((item, index) => (
-                        <ComandaBody
-                            key={index}
-                            titulo={<><span>{item.titulo.split(' ')[0]}</span> {item.titulo.split(' ').slice(1).join(' ')}</>}
-                            descricao={item.descricao}
-                            observacao={item.observacao}
-                        />
-                    ))}
-                </div>
-                
-                <div className="rodape-comanda">
-                    <ComandaFooter />
-                </div>
-            </div>
-        </>
-    )
+      <div className="rodape-comanda">
+        <ComandaFooter />
+      </div>
+    </div>
+  );
 }
-
--- src/pages/Cozinha/Cozinha.jsx
+++ src/pages/Cozinha/Cozinha.jsx
@@ -1,25 +1,69 @@
-import './Cozinha.css'
+import "./Cozinha.css";
 
-import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
-import Comanda from './ComandaFinal/Comanda/Comanda'
+import React, { useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Comanda from "./ComandaFinal/Comanda/Comanda";
 
 export function Cozinha() {
-    return (
-        <>
-            <Navbar/>
-            <section className="cozinha">
-                <header className="titulo">
-                    <h1>Preparo de Pedidos</h1>
-                    <p>8 pedidos</p>
-                </header>
+  const [pedidos, setPedidos] = useState([
+    {
+      numeroPedido: 250,
+      dataHora: "19 Mar 2025, 16:54",
+      itens: [
+        {
+          titulo: "1x Lanche Natural",
+          descricao: "Pão, Alface, Tomate e Molho.",
+          observacao: "SEM TOMATE",
+        },
+        {
+          titulo: "1x Lanche Natural",
+          descricao: "Pão, Alface, Tomate e Molho.",
+        },
+        {
+          titulo: "1x Chicken Jr.",
+          descricao: "Pão de hambúrguer, frango empanado, queijo, presunto...",
+        },
+        {
+          titulo: "1x Lanche Natural",
+          descricao: "Pão, Alface, Tomate e Molho.",
+        }, 
+      ],
+      qtdItens: 4,
+    },
+    {
+      numeroPedido: 251,
+      dataHora: "20 Mar 2025, 16:58",
+      itens: [
+        {
+          titulo: "1x Lanche Natural",
+          descricao: "Pão, Alface, Tomate e Molho.",
+          observacao: "SEM TOMATE",
+        },
+        {
+          titulo: "1x Lanche Natural",
+          descricao: "Pão, Alface, Tomate e Molho.",
+        },
+      ],
+      qtdItens: 2,
+    },
+  ]);
 
-                <article className='tela-comandas'>
-                    <Comanda/>
-                    
-                </article>
+  return (
+    <>
+      <Navbar />
+      <section className="cozinha">
+        <header className="titulo">
+          <h1>Preparo de Pedidos</h1>
+          <p>{pedidos.length === 1 ? pedidos.length + " pedido" : pedidos.length + " pedidos"
+          }</p>
+        </header>
 
-            </section>
-        </>
-    )
+        <article className="tela-comandas">
+          {pedidos.map((pedido, index) => (
+            <Comanda key={index} pedido={pedido} />
+          ))}
+        </article>
+      </section>
+    </>
+  );
 }

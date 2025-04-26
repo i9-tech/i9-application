0246ab581d7674db4c588b1e7b9019799a43f816
GuilhemeBorin <guilherme.galeno@sptech.school>
2025-04-26 20:01:58 -0300

    add: valicação de login e restrição de navegação

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,65 @@
-import { Routes, Route } from 'react-router-dom';
-import { Atendente } from './pages/Atendente/Atendente';
-import { Dashboard } from './pages/Dashboard/Dashboard';
-import Navbar from './components/Navbar/Navbar';
-import './App.css'
-import { Funcionarios } from './pages/Funcionarios/Funcionarios';
-import { Estoque } from './pages/Estoque/Estoque';
-import { Cozinha } from './pages/Cozinha/Cozinha';
-import { Institucional } from './pages/Institucional/Institucional';
-import { Login } from './pages/Login/Login';
+import { Routes, Route } from "react-router-dom";
+import { Atendente } from "./pages/Atendente/Atendente";
+import { Dashboard } from "./pages/Dashboard/Dashboard";
+import "./App.css";
+import { Funcionarios } from "./pages/Funcionarios/Funcionarios";
+import { Estoque } from "./pages/Estoque/Estoque";
+import { Cozinha } from "./pages/Cozinha/Cozinha";
+import { Institucional } from "./pages/Institucional/Institucional";
+import { Login } from "./pages/Login/Login";
+import RotaPrivada from "./routes/RotaPrivada";
+import NaoAutorizado from "./pages/NaoAutorizado/NaoAutorizado";
+
 
 function App() {
 
   return (
     <>
-    <main>
-      <Routes>
-        <Route path='/' element={<Institucional/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/atendente' element={<Atendente/>} />
-        <Route path='/cozinha' element={<Cozinha/>} />
-        <Route path='/estoque' element={<Estoque/>} />
-        <Route path='/funcionarios' element={<Funcionarios/>} />
-      </Routes>
-    </main>
+      <main>
+        <Routes>
+          <Route path="/" element={<Institucional />} />
+          <Route path="/login" element={<Login />} />
+
+          <Route
+            path="/atendente"
+            element={
+              <RotaPrivada permissao="acesso_setor_atendimento">
+                <Atendente />
+              </RotaPrivada>
+            }
+          />
+          <Route
+            path="/cozinha"
+            element={
+              <RotaPrivada permissao="acesso_setor_cozinha">
+                <Cozinha />
+              </RotaPrivada>
+            }
+          />
+          <Route
+            path="/estoque"
+            element={
+              <RotaPrivada permissao="acesso_setor_estoque">
+                <Estoque />
+              </RotaPrivada>
+            }
+          />
+          <Route
+            path="/funcionarios"
+            element={
+              <RotaPrivada permissao="proprietario">
+                <Funcionarios />
+              </RotaPrivada>
+            }
+          />
+          <Route
+            path="/unauthorized"
+            element={<NaoAutorizado />}
+              />
+        </Routes>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

219da552a409034da2e4c6e9fa21a1690d73cef6
m4yk0n <maykon.rodrigues@sptech.school>
2025-04-27 11:43:07 -0300

    feat: url backend e json server

--- src/provider/api.js
+++ src/provider/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000", /* CONSUMIR DADOS DO JSON SERVER */ 
+  baseURL: "http://localhost:8080", /* CONSUMIR DADOS DO BACKEND SPRINGBOOT */
   headers: {
     "Content-Type": "application/json"
   },

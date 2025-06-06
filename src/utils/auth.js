24db677e5871e9804de9a946625c6e644d3eec5f
m4yk0n <maykon.rodrigues@sptech.school>
2025-06-06 19:15:00 -0300

    fix: espaçamento do menu de navegação e posição de opções / feat: login capturando tela interna a partir de permissões

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,5 @@
 import { jwtDecode } from "jwt-decode";
+import { ROUTERS } from "./routers";
 
 export function getFuncionario() {
     const funcionario = localStorage.getItem("funcionario");
@@ -20,6 +21,16 @@ export function getPermissoes() {
 
   return [];
 }
+
+export function getPrimeiraRotaPermitida(permissoes) {
+  if (permissoes.includes("ROLE_PROPRIETARIO")) return ROUTERS.DASHBOARD;
+  if (permissoes.includes("ROLE_ATENDIMENTO")) return ROUTERS.ATENDENTE;
+  if (permissoes.includes("ROLE_COZINHA")) return ROUTERS.COMANDAS;
+  if (permissoes.includes("ROLE_ESTOQUE")) return ROUTERS.ESTOQUE_PRODUTOS;
+
+  return ROUTERS.LOGIN; 
+}
+
 
 export function getToken() {
   const token = localStorage.getItem("token");

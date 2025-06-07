9bef3d9a7585131f8d83db7781a39020d8981bf4
ysilvv <yasmim.silva@sptech.school>
2025-06-07 18:04:39 -0300

    fix: ajustes de css no setor

--- src/components/Hovers/HoverTotalProduto/ElementoTotal.jsx
+++ src/components/Hovers/HoverTotalProduto/ElementoTotal.jsx
@@ -5,7 +5,7 @@ export function ElementoTotal(props) {
     return (
         <>
             <button className="card-total"  onClick={props.onClick}>
-            <img className="lanche" src={props.imagem ? props.imagem : imagemPadrao} alt="Imagem Produto" />
+            <img className="lanche" src={props.imagem ? props.imagem : imagemPadrao} alt="Imagem do Setor" />
                 <span> {props.nome}
                     <small> <p>({props.quantidade} itens) </p>  </small>
                 </span>

3d97c866399a88a4bed3380873014b2df1c1dd6e
m4yk0n <maykon.rodrigues@sptech.school>
2025-06-12 00:17:48 -0300

    style: novo botão de voltar em login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import FormularioLogin from '../../components/FormularioLogin/FormularioLogin';
 import './Login.css';
+import { TbArrowBackUp } from 'react-icons/tb';
 
 export function Login() {
   const navigate = useNavigate();
@@ -8,7 +9,7 @@ export function Login() {
     <section className='login'>
     <span className='login-header'>
     <div className="botoes-log">
-    <button onClick={() => navigate("/")}>X</button>
+    <button onClick={() => navigate("/")}><TbArrowBackUp size={28} /> VOLTAR</button>
     </div>
     <div className='saudacoes'>
     <h1>i9 <br /> Boas Vindas </h1>
